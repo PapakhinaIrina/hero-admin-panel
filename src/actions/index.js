c14332c890eeabb53c3cf5715d65da55eebe40ca
Papakhin Egor <PapakhinEV@msk.bcs.ru>
2022-08-02 16:20:32 +0300

    redux toolkit createAction

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import { createAction } from "@reduxjs/toolkit";
+
 export const fetchHeroes = (request) => (dispatch) => {
     dispatch(heroesFetching());
     request("http://localhost:3001/heroes")
@@ -9,21 +11,25 @@ export const fetchFilters = (request) => (dispatch) => {
     dispatch(filtersFetching());
     request("http://localhost:3001/filters")
         .then(data => dispatch(filtersFetched(data)))
-        .catch(() => dispatch(heroesFetchingError()))
+        .catch(() => dispatch(filtersFetchingError()))
 }
 
-export const heroesFetching = () => {
-    return {
-        type: 'HEROES_FETCHING'
-    }
-}
+// export const heroesFetching = () => {
+//     return {
+//         type: 'HEROES_FETCHING'
+//     }
+// }
 
-export const heroesFetched = (heroes) => {
-    return {
-        type: 'HEROES_FETCHED',
-        payload: heroes
-    }
-}
+export const heroesFetching = createAction('HEROES_FETCHING');
+
+// export const heroesFetched = (heroes) => {
+//     return {
+//         type: 'HEROES_FETCHED',
+//         payload: heroes
+//     }
+// }
+
+export const heroesFetched = createAction('HEROES_FETCHED');
 
 export const heroesFetchingError = () => {
     return {
@@ -50,20 +56,12 @@ export const filtersFetchingError = () => {
     }
 }
 
-export const activeFilterChanged = (filter) => (dispatch) => {
+export const activeFilterChanged = (filter) => {
     return {
-            type: 'ACTIVE_FILTER_CHANGED',
-            payload: filter
-        }  
+        type: 'ACTIVE_FILTER_CHANGED',
+        payload: filter
+    }
 }
-// export const activeFilterChanged = (filter) => (dispatch) => {
-//     setTimeout(() => {
-//         dispatch({
-//             type: 'ACTIVE_FILTER_CHANGED',
-//             payload: filter
-//         })
-//     }, 1000)
-// }
 
 export const heroCreated = (hero) => {
     return {
